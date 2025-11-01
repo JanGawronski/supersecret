da4d5980b92752781563565836dedc29487b0094
JanGawronski <jangawronski04@gmail.com>
2025-11-01 15:14:19 +0100

    Added committing

--- main.sh
+++ main.sh
@@ -10,4 +10,6 @@ if [ -z "${1-}" ]; then
     printf '%s' "$decrypted"
 else
     printf '%s\n%s' "$decrypted" "$1" | gpg --quiet --symmetric --cipher-algo AES256 --batch --passphrase-file passphrase --pinentry-mode loopback -o - | base64 > encrypted.new && mv encrypted.new encrypted
+    git commit -am "Updated secret"
+    git push --quiet
 fi
